b016e8aec52550666d5466c479cf30457d8141f6
farichase <fbazartinova@mail.ru>
2020-10-19 19:52:43 +0300

    Scripted auto-commit on change (2020-10-19 19:52:43) by gitwatch.sh

--- lab2/src/main/java/MapSideJoin.java
+++ lab2/src/main/java/MapSideJoin.java
@@ -1,5 +1,7 @@
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobClient;
 import org.apache.hadoop.mapred.JobConf;
 
 public class MapSideJoin {
@@ -7,9 +9,12 @@ public class MapSideJoin {
         if (args.length != 2) {
             System.exit(-1);
         }
-        JobConf conf = new JobConf(JoinJob.class);
+        JobConf conf = new JobConf(MapSideJoin.class);
         conf.setJobName("map join");
         FileOutputFormat.setOutputPath(conf, new Path(args[2]));
-        conf.serMapperClass(MapJoinMapper.class);
+        conf.setMapperClass(MapJoinMapper.class);
+        conf.setOutputKeyClass(Text.class);
+        conf.setOutputValueClass(Text.class);
+        JobClient.runJob(conf);
     }
 }
