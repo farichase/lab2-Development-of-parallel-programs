b7f38e83d332480d85e4175a35ad6391769ca398
farichase <fbazartinova@mail.ru>
2020-10-19 21:32:25 +0300

    Scripted auto-commit on change (2020-10-19 21:32:25) by gitwatch.sh

--- lab2/src/main/java/MapJoinMapper.java
+++ lab2/src/main/java/MapJoinMapper.java
@@ -1,5 +1,10 @@
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
 
-public class MapJoinMapper extends Mapper<LongWritable, Text, Text, > {
+public class MapJoinMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    public MapJoinMapper() {
+        super();
+    }
 }
