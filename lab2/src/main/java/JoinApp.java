f63ec7bfeae7d1668f4c9588e48458c64ec23cf3
farichase <fbazartinova@mail.ru>
2020-10-19 21:53:08 +0300

    Scripted auto-commit on change (2020-10-19 21:53:08) by gitwatch.sh

--- lab2/src/main/java/JoinApp.java
+++ lab2/src/main/java/JoinApp.java
@@ -1,19 +1,17 @@
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 
-import java.util.List;
-
-public class MapSideJoin {
+public class JoinApp {
     public static void main (String[] args) throws Exception{
         if (args.length != 2) {
             System.exit(-1);
         }
-        JobConf conf = new JobConf(MapSideJoin.class);
+        JobConf conf = new JobConf(JoinApp.class);
         conf.setJobName("map join");
         FileOutputFormat.setOutputPath(conf, new Path(args[2]));
-        TextInputFormat inputFormat = new TextInputFormat();
-        InputSplit[] splits = inputFormat.getSplits(conf);
+        MultipleInputs(conf);
         conf.setMapperClass(MapJoinMapper.class);
         conf.setOutputKeyClass(Text.class);
         JobClient.runJob(conf);
