f134a5977a9df5bd4152e26a018e7640306953c7
farichase <fbazartinova@mail.ru>
2020-10-19 22:03:31 +0300

    Scripted auto-commit on change (2020-10-19 22:03:31) by gitwatch.sh

--- lab2/src/main/java/JoinApp.java
+++ lab2/src/main/java/JoinApp.java
@@ -1,6 +1,7 @@
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.mapred.jobcontrol.Job;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 
 public class JoinApp {
@@ -8,12 +9,11 @@ public class JoinApp {
         if (args.length != 3) {
             System.exit(-1);
         }
-        JobConf conf = new JobConf(JoinApp.class);
-        conf.setJobName("JoinApp");
-        MultipleInputs.addInputPath(conf, new Path(args[0]), TextInputFormat.class, FlightMapper.class);
-        MultipleInputs.addInputPath(conf, new Path(args[1]), TextInputFormat.class, AirportMapper.class);
-        FileOutputFormat.setOutputPath(conf, new Path(args[2]));
-        conf.setOutputKeyClass(Text.class);
-        JobClient.runJob(conf);
+        Job job = new Job();
+        job.setJobName("JoinApp");
+        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FlightMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, AirportMapper.class);
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
+        JobClient.runJob(job);
     }
 }
