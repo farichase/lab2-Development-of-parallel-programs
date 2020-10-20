c939847cf1e3c6b64cca211a19e6e351bf62a34a
farichase <fbazartinova@mail.ru>
2020-10-20 13:51:22 +0300

    Scripted auto-commit on change (2020-10-20 13:51:22) by gitwatch.sh

--- lab2/src/main/java/AirportMapper.java
+++ lab2/src/main/java/AirportMapper.java
@@ -9,6 +9,6 @@ public class AirportMapper extends Mapper<LongWritable, Text, Text, IntWritable>
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String[] lines = value.toString().split(",");
-        
+
     }
 }
