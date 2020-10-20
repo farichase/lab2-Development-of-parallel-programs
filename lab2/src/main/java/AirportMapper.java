2481bfd6077a403e439b7a4a8c02dd71be8c1062
farichase <fbazartinova@mail.ru>
2020-10-20 16:31:49 +0300

    Scripted auto-commit on change (2020-10-20 16:31:49) by gitwatch.sh

--- lab2/src/main/java/AirportMapper.java
+++ lab2/src/main/java/AirportMapper.java
@@ -1,26 +1,25 @@
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
 
 public class AirportMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
-    private static int AEROPORT_ID = 14;
-    private static int ARR_DELAY = 18;
+    private static int AIRPORT_ID = 14;
+    private static int AIRPORT_DELAY = 18;
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String[] line = value.toString().split(",");
         if (key.get() > 0){
             float delay = 0.f;
-            if (line[ARR_DELAY].trim().length() != 0) {
-                delay = Float.parseFloat(line[ARR_DELAY]);
+            if (line[AIRPORT_DELAY].trim().length() != 0) {
+                delay = Float.parseFloat(line[AIRPORT_DELAY]);
             }
             if (delay > 0) {
                 context.write(
-                        new AirportWritableComparable(Integer.parseInt(line[AEROPORT_ID]), 1),
-                        new Text(line[ARR_DELAY]));
+                        new AirportWritableComparable(Integer.parseInt(line[AIRPORT_ID]), 1),
+                        new Text(line[AIRPORT_DELAY]));
             }
         }
     }
