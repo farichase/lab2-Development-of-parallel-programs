b659f24ff84cfa6a15b0b3b991daf74546cc9c16
farichase <fbazartinova@mail.ru>
2020-10-20 16:05:30 +0300

    Scripted auto-commit on change (2020-10-20 16:05:29) by gitwatch.sh

--- lab2/src/main/java/AirportMapper.java
+++ lab2/src/main/java/AirportMapper.java
@@ -12,8 +12,10 @@ public class AirportMapper extends Mapper<LongWritable, Text, Text, IntWritable>
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String[] line = value.toString().split(",");
         if (key.get() > 0){
-            if ()
-            int delay = Integer.parseInt(line[ARR_DELAY]);
+            float delay = 0.f;
+            if (line[ARR_DELAY].trim().length() != 0) {
+                delay = Float.parseFloat(line[ARR_DELAY]);
+            }
             if (delay > 0) {
                 //context.write(new , new Text(line[ARR_DELAY]));
             }
