0944a7acc9eaed673657f83898c3da6105b9fd76
farichase <fbazartinova@mail.ru>
2020-10-20 14:08:31 +0300

    Scripted auto-commit on change (2020-10-20 14:08:31) by gitwatch.sh

--- lab2/src/main/java/AirportMapper.java
+++ lab2/src/main/java/AirportMapper.java
@@ -6,12 +6,13 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 public class AirportMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    private static int AEROPORT_ID = 14;
+    private static int ARR_DELAY = 18;
     @Override
-    private int AEROPORT_ID;
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String[] lines = value.toString().split(",");
         if (key.get() > 0){
-
+            
         }
     }
 }
