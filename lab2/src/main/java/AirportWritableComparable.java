cbb3f761980c156e36ec60ca7f84dc3c63db0726
farichase <fbazartinova@mail.ru>
2020-10-20 16:37:14 +0300

    Scripted auto-commit on change (2020-10-20 16:37:14) by gitwatch.sh

--- lab2/src/main/java/AirportWritableComparable.java
+++ lab2/src/main/java/AirportWritableComparable.java
@@ -2,6 +2,7 @@ import org.apache.hadoop.io.WritableComparable;
 
 import java.io.DataInput;
 import java.io.DataOutput;
+import java.io.IOException;
 
 public class AirportWritableComparable implements WritableComparable<AirportWritableComparable> {
     private int aeroport_id;
@@ -11,13 +12,13 @@ public class AirportWritableComparable implements WritableComparable<AirportWrit
         this.indicator = indicator;
     }
     @Override
-    public void write(DataOutput data) throws  {
+    public void write(DataOutput data) throws IOException {
         data.writeInt(aeroport_id);
         data.writeInt(indicator);
     }
     @Override
-    public void readFields(DataInput data){
-
+    public void readFields(DataInput data) throws IOException{
+        data
     }
     @Override
     public int compareTo(AirportWritableComparable item){
