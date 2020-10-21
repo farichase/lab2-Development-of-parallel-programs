87ab95138e6cf71aee1e3b692d97e32eca7366a8
farichase <fbazartinova@mail.ru>
2020-10-21 20:00:58 +0300

    Scripted auto-commit on change (2020-10-21 20:00:58) by gitwatch.sh

--- lab2/src/main/java/AirportsReducer.java
+++ lab2/src/main/java/AirportsReducer.java
@@ -1,2 +1,12 @@
-public class AirportsReducer {
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class AirportsReducer extends Reducer<Text, IntWritable, Text, LongWritable> {
+    @Override
+    protected void reduce(LongWritable key, Text value, Mapper.Context context){
+        
+    }
 }
