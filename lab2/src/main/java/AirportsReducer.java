266725661fdcc8b8b48016184fce8e82ada90a82
farichase <fbazartinova@mail.ru>
2020-10-21 20:04:37 +0300

    Scripted auto-commit on change (2020-10-21 20:04:37) by gitwatch.sh

--- lab2/src/main/java/AirportsReducer.java
+++ lab2/src/main/java/AirportsReducer.java
@@ -1,14 +1,17 @@
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.util.Iterator;
 
 public class AirportsReducer extends Reducer<Text, IntWritable, Text, LongWritable> {
     @Override
-    protected void reduce(FlightsWritableComparable key, Iterable<Text> value, Context context){
-        Iterator
+    protected void reduce(FlightsWritableComparable key, Iterable<Text> values, Context context){
+        Iterator<Text> iter = values.iterator();
+        Text systemInfo = new Text(iter.next());
+        while (iter.hasNext()) {
+
+        }
     }
 }
