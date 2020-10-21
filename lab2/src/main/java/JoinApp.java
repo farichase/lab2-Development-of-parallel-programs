6d16d66d7f0aa9ca59fd0fef36f2060bf69432f6
farichase <fbazartinova@mail.ru>
2020-10-21 15:01:59 +0300

    Scripted auto-commit on change (2020-10-21 15:01:59) by gitwatch.sh

--- lab2/src/main/java/JoinApp.java
+++ lab2/src/main/java/JoinApp.java
@@ -1,9 +1,9 @@
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.mapred.FileOutputFormat;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class JoinApp {
     public static void main (String[] args) throws Exception{
