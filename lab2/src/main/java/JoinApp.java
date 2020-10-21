13f4a7a2903bab9913d951da8b4f2320972e58ce
farichase <fbazartinova@mail.ru>
2020-10-21 14:56:56 +0300

    Scripted auto-commit on change (2020-10-21 14:56:56) by gitwatch.sh

--- lab2/src/main/java/JoinApp.java
+++ lab2/src/main/java/JoinApp.java
@@ -1,5 +1,6 @@
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapred.FileOutputFormat;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
@@ -13,6 +14,6 @@ public class JoinApp {
         Job job = Job.getInstance(conf, "JoinApp");
         MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FlightsMapper.class);
         MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, AirportsMapper.class);
-        File.
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
     }
 }
