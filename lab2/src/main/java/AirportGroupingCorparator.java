81f3718ffa772df957d93e97d8f901dddcfaf55f
farichase <fbazartinova@mail.ru>
2020-10-21 19:25:01 +0300

    Scripted auto-commit on change (2020-10-21 19:25:00) by gitwatch.sh

--- lab2/src/main/java/AirportGroupingCorparator.java
+++ lab2/src/main/java/AirportGroupingCorparator.java
@@ -1,2 +1,8 @@
-public class AirportGroupingCorparator {
+import org.apache.hadoop.io.RawComparator;
+
+public class AirportGroupingCorparator implements RawComparator {
+
+    public int compare(FlightsWritableComparable a, FlightsWritableComparable b){
+        return a.compareTo(b);
+    }
 }
