211ee581f0846c26156f5e96820861cb370f49f7
farichase <fbazartinova@mail.ru>
2020-10-21 15:01:10 +0300

    Scripted auto-commit on change (2020-10-21 15:01:09) by gitwatch.sh

--- lab2/src/main/java/JoinApp.java
+++ lab2/src/main/java/JoinApp.java
@@ -11,7 +11,9 @@ public class JoinApp {
             System.exit(-1);
         }
         Configuration conf = new Configuration();
-        Job job = Job.getInstance(conf, "JoinApp");
+        Job job = Job.getInstance(conf);
+        job.setJarByClass(JoinApp.class);
+        job.setJobName("JoinApp");
         MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FlightsMapper.class);
         MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, AirportsMapper.class);
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
