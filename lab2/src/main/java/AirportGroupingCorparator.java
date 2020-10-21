3376389dee927a2765a17e089ffdb6ae21726ae3
farichase <fbazartinova@mail.ru>
2020-10-21 19:51:59 +0300

    Scripted auto-commit on change (2020-10-21 19:51:59) by gitwatch.sh

--- lab2/src/main/java/AirportGroupingCorparator.java
+++ lab2/src/main/java/AirportGroupingCorparator.java
@@ -1,8 +1,14 @@
+import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.io.WritableComparator;
 
 public class AirportGroupingCorparator extends WritableComparator {
+    public AirportGroupingCorparator() {
+        super(FlightsWritableComparable.class, true);
+    }
     @Override
-    public int compare(FlightsWritableComparable a, FlightsWritableComparable b){
-        return a.compareTo(b);
+    public int compare(WritableComparable a, WritableComparable b){
+        FlightsWritableComparable a1 = (FlightsWritableComparable) a;
+        FlightsWritableComparable b1 = (FlightsWritableComparable) b;
+        return a1.compareTo(b1);
     }
 }
