1a8a9915c51375faee440fa99aae4403858738f3
farichase <fbazartinova@mail.ru>
2020-10-23 19:08:10 +0300

    Scripted auto-commit on change (2020-10-23 19:08:10) by gitwatch.sh

--- lab2/src/main/java/AirportsReducer.java
+++ lab2/src/main/java/AirportsReducer.java
@@ -12,9 +12,9 @@ public class AirportsReducer extends Reducer<FlightsWritableComparable, Text, Te
             throws IOException, InterruptedException {
         Iterator<Text> iter = values.iterator();
         Text airport_name = iter.next();
-        int quantity = 0;
-        float sum = 0, min = MIN, max = MAX;
         if (iter.hasNext()) {
+            int quantity = 0;
+            float sum = 0, min = MIN, max = MAX;
             String delay_str = String.valueOf(iter.next());
             float delay = Float.parseFloat(delay_str);
             System.out.println(delay);
