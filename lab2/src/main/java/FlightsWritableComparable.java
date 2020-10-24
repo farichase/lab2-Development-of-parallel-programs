271ccb4026272b8cfd620225770c0ccc0051f5f1
farichase <fbazartinova@mail.ru>
2020-10-24 14:47:14 +0300

    Scripted auto-commit on change (2020-10-24 14:47:14) by gitwatch.sh

--- lab2/src/main/java/FlightsWritableComparable.java
+++ lab2/src/main/java/FlightsWritableComparable.java
@@ -5,47 +5,47 @@ import java.io.DataOutput;
 import java.io.IOException;
 
 public class FlightsWritableComparable implements WritableComparable<FlightsWritableComparable> {
-    private int aeroport_id;
+    private int airport_id;
     private int indicator;
-    public FlightsWritableComparable(int aeroport_id, int indicator){
-        this.aeroport_id = aeroport_id;
+    public FlightsWritableComparable(int airport_id, int indicator){
+        this.airport_id = airport_id;
         this.indicator = indicator;
     }
     public FlightsWritableComparable(){
-        this.aeroport_id = 0;
+        this.airport_id = 0;
         this.indicator = 0;
     }
     @Override
     public void write(DataOutput data) throws IOException {
-        data.writeInt(this.aeroport_id);
+        data.writeInt(this.airport_id);
         data.writeInt(this.indicator);
     }
     @Override
     public void readFields(DataInput data) throws IOException{
-        this.aeroport_id = data.readInt();
+        this.airport_id = data.readInt();
         this.indicator = data.readInt();
     }
     public void setAeroport_id(int aeroport_id){
-        this.aeroport_id = aeroport_id;
+        this.airport_id = aeroport_id;
     }
     public void setIndicator(int indicator){
         this.indicator = indicator;
     }
     public int getAeroport_id(){
-        return this.aeroport_id;
+        return this.airport_id;
     }
     public int getIndicator(){
         return this.indicator;
     }
     @Override
     public int compareTo(FlightsWritableComparable item){
-        if (this.aeroport_id > item.getAeroport_id())
+        if (this.airport_id > item.airport_id)
             return 1;
-        else if (this.aeroport_id != item.getAeroport_id())
+        else if (this.airport_id != item.airport_id)
             return -1;
-        if (this.indicator > item.getIndicator())
+        if (this.indicator > item.indicator)
             return 1;
-        else if (this.indicator != item.getIndicator())
+        else if (this.indicator != item.indicator)
             return -1;
         return 0;
     }
