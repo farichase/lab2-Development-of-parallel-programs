f258effed2d0180baec271bc004f952975c402c7
farichase <fbazartinova@mail.ru>
2020-10-24 15:33:45 +0300

    Scripted auto-commit on change (2020-10-24 15:33:45) by gitwatch.sh

--- lab2/src/main/java/AirportsMapper.java
+++ lab2/src/main/java/AirportsMapper.java
@@ -11,9 +11,11 @@ public class AirportsMapper extends Mapper<LongWritable, Text, Text, IntWritable
         String[] line = value.toString().split(",[\"]");
         if (key.get() > 0){
             String airport_id = line[0].replaceAll("\"", "");
+            String airport_name = line[1].replaceAll("\"", "");
+            System.out.println(airport_name);
             context.write(
                     new FlightsWritableComparable(Integer.parseInt(airport_id), 0),
-                    new Text(line[1].replaceAll("\"", ""))
+                    new Text(airport_name)
             );
         }
     }
