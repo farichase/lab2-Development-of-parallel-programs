7b895d6635e94a643a23c6780dc59ad67a0bb1c3
farichase <fbazartinova@mail.ru>
2020-10-24 15:30:09 +0300

    Scripted auto-commit on change (2020-10-24 15:30:09) by gitwatch.sh

--- lab2/src/main/java/AirportsMapper.java
+++ lab2/src/main/java/AirportsMapper.java
@@ -10,14 +10,10 @@ public class AirportsMapper extends Mapper<LongWritable, Text, Text, IntWritable
     protected void map(LongWritable key, Text value, Mapper.Context context) throws IOException, InterruptedException {
         String[] line = value.toString().split(",[\"]");
         if (key.get() > 0){
-            String airport_name = "";
-            for (int i = 1; i < line.length; i++) {
-                airport_name += line[i];
-            }
             String airport_id = line[0].replaceAll("\"", "");
             context.write(
                     new FlightsWritableComparable(Integer.parseInt(airport_id), 0),
-                    new Text(airport_name)
+                    new Text(line[1].replaceAll("\"", ""))
             );
         }
     }
