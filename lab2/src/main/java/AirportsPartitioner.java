2435d0c5a33b0f99438bb66f5613dc195cc35c3b
farichase <fbazartinova@mail.ru>
2020-10-26 12:03:40 +0300

    Scripted auto-commit on change (2020-10-26 12:03:40) by gitwatch.sh

--- lab2/src/main/java/AirportsPartitioner.java
+++ lab2/src/main/java/AirportsPartitioner.java
@@ -2,7 +2,8 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Partitioner;
 
 public class AirportsPartitioner extends Partitioner<FlightsWritableComparable, Text> {
+    @Override
     public int getPartition(FlightsWritableComparable key, Text value, int numReduceTasks){
-        return (key.hashCode() & Integer.MAX_VALUE) % numReduceTasks;
+        return (key.getAeroport_id() & Integer.MAX_VALUE) % numReduceTasks;
     }
 }
